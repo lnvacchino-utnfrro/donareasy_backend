f37c7b13ccc46c84617d5178a06d73c43cac5b66
Lucas Vacchino <lnvacchino@gmail.com>
2022-01-10 23:14:05 -0300

    Desarrollo de test para Donantes (es una prueba de como se hace)

--- login/tests.py
+++ login/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from login.models import Donante
+from serializers import DonanteSerializer
 
-# Create your tests here.
+class DonantesLoginTests(APITestCase):
+
+    def test_crear_donante(self):
+        """
+        Crear un donante y verificar que exista
+        """
+        url = reverse('donantes-list')
+        data = {
+            'nombre': 'juansito', 
+            'apellido':'janterini', 
+            'email':'juan.jan@gmail.com',
+            'edad':11
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Donante.objects.count(), 1)
