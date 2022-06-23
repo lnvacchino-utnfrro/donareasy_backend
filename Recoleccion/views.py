0584f163b58e66f67c1598b75078e8efb3803099
Lucas Vacchino <lnvacchino@gmail.com>
2022-06-23 19:30:13 -0300

    Listado de recolecciones

--- Recoleccion/views.py
+++ Recoleccion/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+
+from Recoleccion.models import Recoleccion
+from Recoleccion.serializers import RecoleccionSerializer
+
+class DonanteList(generics.ListAPIView):
+    """APIView para listar las recolecciones creadas"""
+    queryset = Recoleccion.objects.all()
+    serializer_class = RecoleccionSerializer
