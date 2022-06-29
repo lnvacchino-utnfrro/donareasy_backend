aec2efe9c80583f96167b040946f8942b89a570e
Lucas Vacchino <lnvacchino@gmail.com>
2022-06-28 23:50:28 -0300

    Corrección para el create de recoleccion

--- Recoleccion/serializers.py
+++ Recoleccion/serializers.py
@@ -1,3 +1,4 @@
+from django.forms import IntegerField
 from rest_framework import serializers
 from DonacionesApp.models import DonacionBienes, Bien, DonacionMonetaria
 from Recoleccion.models import *
@@ -7,31 +8,24 @@ from datetime import datetime,date
 from DonacionesApp.serializers import DonacionBienesSerializer, IdDonacionSerializer
 
 class RecoleccionesCreateSerializer(serializers.ModelSerializer):
-     #donacion = DonacionBienesSerializer(many=True)
-     donaciones = serializers.ListField( #IdDonacionSerializer(many=True)
-            child= IdDonacionSerializer(many=True))
-     #cadete = CadeteSerializer
-     class Meta:
-         model = Recoleccion
-         fields = ['cadete','fecha_recoleccion','hora_recoleccion','donaciones']
-         read_only_fields = ['estado_recoleccion']
+    serializers.PrimaryKeyRelatedField(many=True, queryset=DonacionBienes.objects.all())
 
-     def create(self,validated_data):
+    class Meta:
+        model = Recoleccion
+        fields = ['cadete','fecha_recoleccion','hora_recoleccion','donaciones']
+        read_only_fields = ['estado_recoleccion']
+
+    def create(self,validated_data):
         donaciones_data = validated_data.pop('donaciones')
         recoleccion = Recoleccion.objects.create(
-             cadete = validated_data['cadete'],
-             fecha_recoleccion = validated_data['fecha_recoleccion'],
-             hora_recoleccion = validated_data['hora_recoleccion'],
-             estado_recoleccion = 1     
+            cadete = validated_data['cadete'],
+            fecha_recoleccion = validated_data['fecha_recoleccion'],
+            hora_recoleccion = validated_data['hora_recoleccion'],
+            estado_recoleccion = 1     
         )
         recoleccion.save() 
-        for donacion_data in donaciones_data:
-            #donacion = DonacionBienes.objects.get(pk=donacion_data, **donacion_data)
-            print(donacion_data)
-            donacion = DonacionBienes.objects.get(id=donacion_data, **donacion_data) #si donacion_data es un numero
-                                                                   #sino, ver como conseguir el número ingresado en la lista
-            # https://docs.djangoproject.com/en/4.0/topics/db/queries/
+        for donacion in donaciones_data:
             donacion.recoleccion = recoleccion
             donacion.save()
-
+            
         return recoleccion
