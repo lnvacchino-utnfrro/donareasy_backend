653588fd88d047986c9ed8a8a3270161c59d1725
Andrés Botello <andresjbotello@gmail.com>
2022-12-05 20:38:17 -0300

    CORS

--- donareasy/settings.py
+++ donareasy/settings.py
@@ -62,6 +62,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True  
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
+
 ROOT_URLCONF = 'donareasy.urls'
 
 TEMPLATES = [
